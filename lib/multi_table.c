054ec3cb47c821e58d5f41b261bb9dc2cdd42dc9
Elvin <elvin.bayramov@protonmail.com>
2021-10-21 16:56:51 +0400

    Hot fix

--- lib/multi_table.c
+++ lib/multi_table.c
@@ -3,6 +3,7 @@
 //
 #ifndef MULTI_TABLE_C
 #define MULTI_TABLE_C
+
 #include "table.c"
 
 /**
@@ -14,97 +15,98 @@
  * */
 struct TableDescriptor {
     int index;
-    struct Table* table;
+    struct Table *table;
 };
 
 /**
  * Data structure for containing several matrices with keys as indices
  * */
-struct MultiTable{
-    struct TableDescriptor* tables;
+struct MultiTable {
+    struct TableDescriptor *tables;
     size_t size;
 };
 
 /**
  * Function for finding a table by index. Returns NULL if table doesn't exists
  * */
-struct Table* find_table(struct MultiTable multiTable, int index);
+struct Table *find_table(struct MultiTable multiTable, int index);
 
 /**
  * Function for generating a multi-table for generating multi-table
  * It's crucial that indices and tables have equal length
  * */
-struct MultiTable generate_multi_table(struct Table* tables, struct Array indices);
+struct MultiTable generate_multi_table(struct Table *tables, struct Array indices);
 
 /**
  * Function for printing the content of multi-table
  * */
-void print_multi_table(struct MultiTable* multi_table);
+void print_multi_table(struct MultiTable *multi_table);
 
 /**
  * Function for writing multi-table to .csv file
  * */
-void save_multi_table_in_file(const char* path, struct MultiTable* multi_table);
+void save_multi_table_in_file(const char *path, struct MultiTable *multi_table);
 
 /**
  * Implementations
  * */
-struct Table* find_table(struct MultiTable multiTable, int index) {
-    for (int i = 0; i < multiTable.size; i++) {
-        if (multiTable.tables[i].index == index) {
-            return multiTable.tables[i].table;
-        }
+struct Table *find_table(struct MultiTable multiTable, int index) {
+  for (int i = 0; i < multiTable.size; i++) {
+    if (multiTable.tables[i].index == index) {
+      return multiTable.tables[i].table;
     }
+  }
 
-    return NULL;
+  return NULL;
 }
 
-struct MultiTable generate_multi_table(struct Table* tables, struct Array indices) {
-    size_t n = indices.size;
+struct MultiTable generate_multi_table(struct Table *tables, struct Array indices) {
+  size_t n = indices.size;
 
-    struct TableDescriptor* descriptors = (struct TableDescriptor*)malloc(sizeof(struct TableDescriptor) * n);
+  struct TableDescriptor *descriptors = (struct TableDescriptor *) malloc(sizeof(struct TableDescriptor) * n);
 
-    for (int i = 0; i < n; i++) {
-        descriptors[i] = { .index=(int)indices.values[i], .table=(tables + i) };
-    }
+  for (int i = 0; i < n; i++) {
+    struct TableDescriptor descriptor = {.index=(int) indices.values[i], .table=(tables + i)};
+    descriptors[i] = descriptor;
+  }
 
-    struct MultiTable result = {
-        descriptors,
-        n
-    };
+  struct MultiTable result = {
+      descriptors,
+      n
+  };
 
-    return result;
+  return result;
 }
 
-void print_multi_table(struct MultiTable* multi_table) {
-    if (multi_table == NULL)
-        return;
+void print_multi_table(struct MultiTable *multi_table) {
+  if (multi_table == NULL)
+    return;
 
-    int i;
+  int i;
 
-    struct TableDescriptor descriptor;
-    printf("----Start printing multi-table----\n");
-    for (i = 0; i < multi_table->size; i++) {
-        descriptor = multi_table->tables[i];
+  struct TableDescriptor descriptor;
+  printf("----Start printing multi-table----\n");
+  for (i = 0; i < multi_table->size; i++) {
+    descriptor = multi_table->tables[i];
 
-        printf("Index: %i\n", descriptor.index);
+    printf("Index: %i\n", descriptor.index);
 
-        print_table(descriptor.table);
-    }
-    printf("----End printing multi-table----\n");
+    print_table(descriptor.table);
+  }
+  printf("----End printing multi-table----\n");
 }
 
-void save_multi_table_in_file(const char* path, struct MultiTable* multi_table) {
+void save_multi_table_in_file(const char *path, struct MultiTable *multi_table) {
   if (multi_table == NULL || multi_table->size == 0 || multi_table->tables == NULL) {
     return;
   }
 
   struct Array header = multi_table->tables[0].table->headers;
 
-  char* file_name = generate_filename("temp/write_multi_table_%li.csv");
+  char *file_name = generate_filename("temp/write_multi_table_%li.csv");
 
   // Open file
-  FILE* file = fopen(file_name, "w");
+  FILE *file = fopen(file_name, "w");
 
   if (!file) {
     return;
@@ -113,7 +115,7 @@ void save_multi_table_in_file(const char* path, struct MultiTable* multi_table) 
   int i, j, index;
   // Write header
   fprintf(file, "p;");
-  for(i = 0; i < header.size; i++)
+  for (i = 0; i < header.size; i++)
     fprintf(file, ";%lf", header.values[i]);
   fprintf(file, "\n");
 
